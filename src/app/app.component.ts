6751fac96c64f71656672673a94bb795ae2710a8
Surabhi <surabhigeorge123@gmail.com>
2020-06-13 20:07:24 +0530

    Slider Images Added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,13 +15,13 @@ export class AppComponent {
     // slider screens
     slider_screens = [
         {
-            photo: 'https://data.whicdn.com/images/303284270/original.jpg?t=1513309244',
+            photo: 'slide1.jpg',
         },            
         {
-            photo: 'https://lh3.googleusercontent.com/proxy/WxqX-aIgsGZPJwYcJqImv7YvbxSmzV6LVoH6XVb153-FHpiXVdAksBEyrsXR9o5-fm5Pk8EVT_moCwVxTmNE881wSPCab-DUrofvkYYOJFvKEQTsbxqNBB0lFi0G7bQvfDuh4co5oVeNWeyL6fb3SB7Gi8yWyUC2Q3LrnUrp4A_-sKN2CG5U-hoHNQ',
+            photo: 'slide2.jpg',
         },
         {
-            photo: 'https://i.pinimg.com/originals/62/76/e8/6276e82ccfb617ddd26973056234ef9e.jpg',
+            photo: 'slide3.jpg',
         }
         
     ]
